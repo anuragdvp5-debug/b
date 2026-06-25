f0f82f4e5ba7bbe45c6406d6f259f356062dfe64
anuragdvp5-debug <anuragdvp5@gmail.com>
2026-06-25 08:26:10 +0530

    index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.post('/connect', (req, res) => {
     res.json({
         "status": true,
         "data": {
-            "real": "key-@toxicteamofc-1acc74bd6862522a3c08c21371265bffa676e6c9d8789d673a873e9d4e3e0ce3-Vm8Lk7Uj2JmsjCPVPVjrLa7zgfx3uz9E",
+            "real": "key-anurag-1acc74bd6862522a3c08c21371265bffa676e6c9d8789d673a873e9d4e3e0ce3-Vm8Lk7Uj2JmsjCPVPVjrLa7zgfx3uz9E",
             "token": "8117e9b001fb568b9279eccf5a64e08d",
             "modname": "Aryanispe Related",
             "mod_status": "Safe",
