9a4a60dd831a2dd24e97fbed64dc57e747026c82
anuragdvp5-debug <anuragdvp5@gmail.com>
2026-06-26 10:20:50 +0530

    index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const DB_FILE = './database.json';
 // Initial data load
 let KEYS = fs.existsSync(DB_FILE) ? JSON.parse(fs.readFileSync(DB_FILE)) : {
     "ANURAG": { type: "pro", active: true, expiry: "2026-06-28", lockedDevice: null },
-    "SACHIN": { type: "pro", active: true, expiry: "2026-06-28", lockedDevice: null },
+    "SACHIN": { type: "pro", active: true, expiry: "2026-06-27", lockedDevice: null },
     "kapil bhai": { type: "pro", active: true, expiry: "2026-06-28", lockedDevice: null },
     "vineet": { type: "pro", active: true, expiry: "2026-06-28", lockedDevice: null },
     
