930cdc5eb6502661d893d88b6c6fa6539c7f819b
anuragdvp5-debug <anuragdvp5@gmail.com>
2026-06-27 11:39:43 +0530

    index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let KEYS = fs.existsSync(DB_FILE) ? JSON.parse(fs.readFileSync(DB_FILE)) : {
     "ANURAG": { type: "pro", active: true, expiry: "2026-06-28", lockedDevice: null },
     "SACHIN": { type: "pro", active: true, expiry: "2026-06-26", lockedDevice: null },
     
-    "ANURAG-BALAJI": { type: "trial", active: true, expiry: "2026-06-28", maxDevices: 500, usedDevices: [] }
+    "TRIAL": { type: "trial", active: true, expiry: "2026-06-28", maxDevices: 500, usedDevices: [] }
 };
 
 // Data save karne ka function
