3bb4ec0faa1edef7258b75839f118c6bad08fef5
anuragdvp5-debug <anuragdvp5@gmail.com>
2026-06-28 15:37:27 +0530

    index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const DB_FILE = './database.json';
 let KEYS = fs.existsSync(DB_FILE) ? JSON.parse(fs.readFileSync(DB_FILE)) : {
     "ANURAG": { type: "pro", active: true, expiry: "2026-07-26", lockedDevice: null },
     "SACHIN": { type: "pro", active: true, expiry: "2026-07-26", lockedDevice: null },
+    "VALI MODS": { type: "pro", active: true, expiry: "2026-06-28", lockedDevice: null },
      
     
     "TRIAL": { type: "trial", active: true, expiry: "2026-06-28", maxDevices: 500, usedDevices: [] }
