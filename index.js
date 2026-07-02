569bb98866d3f5dad7802559db074bb376c707be
anuragdvp5-debug <anuragdvp5@gmail.com>
2026-07-02 21:04:20 +0530

    index.js

--- index.js
+++ index.js
@@ -67,18 +67,19 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 
 // --- DUSRE INJECTOR (b2k) KE LIYE CONTROL CODE ---
-
 app.post('/c/b2k', (req, res) => {
-    // App ke bheje hue data ko echo back karo agar zaruri ho
+    // Ye headers exact match karo
+    res.setHeader('Content-Type', 'application/json; charset=utf-8');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Etag', 'W/"54-Kc+TiHtTsAkVMcWPe01lrhKfW2k"'); // Yeh header 222_2.PNG se hai
+    
     res.status(200).json({
         "status": true,
         "data": {
             "token": "cf38faf2dbe3587f85f11df824cc65a8",
-            "rng": 178300561,
-            "user_key": req.body.user_key // Kuch apps apna bheja hua key wapas expect karti hain
+            "rng": 178300561
         }
     });
 });
-    
 
 
