034c6fc2a048c74a342da97301a1ede2992f5df4
anuragdvp5-debug <anuragdvp5@gmail.com>
2026-07-02 17:46:02 +0530

    index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let KEYS = fs.existsSync(DB_FILE) ? JSON.parse(fs.readFileSync(DB_FILE)) : {
     "ANURAG": { type: "pro", active: true, expiry: "2026-07-26", lockedDevice: null },
     "SACHIN": { type: "pro", active: true, expiry: "2026-07-26", lockedDevice: null },
     
-
+    "MD": { type: "pro", active: true, expiry: "2026-07-02", lockedDevice: null },
 
     "ANURAG1": { type: "pro", active: true, expiry: "2026-07-29", lockedDevice: null },
 
